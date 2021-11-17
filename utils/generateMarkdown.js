c191cd5a4d6dbe96fea9e2122ad34a492da6f96f
Edelgard <jtboyman@gmail.com>
2021-11-17 17:20:36 -0500

    deconstruct variables to set up special exception functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenses) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenses) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenses) {}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data => {
+
+  const {projectLink, screenshot, licenses, ...data};
+
   return `
   # ${data.projectName}
   (badge placeholder)
