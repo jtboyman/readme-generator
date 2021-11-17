d8e77dc9b22763483390ba64f6868b53fb5f92fa
Edelgard <jtboyman@gmail.com>
2021-11-17 16:28:33 -0500

    create questions for user input, set up readme file format, attempt writefile method

---
+++ index.js
@@ -0,0 +1,129 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('.utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a name for the project!');
+                return false;
+            };
+        },
+    },
+    {
+        type: 'input',
+        name: 'projectDesc',
+        message: 'Please enter a description of your project. (Required)',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please add a description!');
+                return false;
+            };
+        },
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLink',
+        message: 'Would you like to provide a link to the deployed application?',
+        default: false,
+    },
+    {
+        type: 'input',
+        name: 'projectLink',
+        message: 'Provide a link to your deployed project.',
+        when: ({confirmLink}) => {
+            if (confirmLink) {
+                return true;
+            } else {
+                return false;
+            };
+        },
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Provide steps for installing your app:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for usage:',
+    },
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to include a screenshot of your application?',
+        default: false,
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Provide a link or relative path to a screenshot of the application:',
+        when: ({confirmUsage}) => {
+            if (confirmUsage) {
+                return true;
+            } else {
+                return false;
+            };
+        },
+    },
+    {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'Choose a license:',
+        choices: ['MIT', 'GNU General Public License v3.0', 'Other/No License'],
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Add guidelines for contributing to your project:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide any tests or code examples of your app:',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Add your GitHub username so users can contact you:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address so users can contact you:',
+    },
+];
+
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise((resolve,reject) => {
+        fs.writeFile('./dist/README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,51 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+const generateMarkdown = data => {
+  return `
+  # ${data.projectName}
+  (badge placeholder)
+
+  ## Table of Contents:
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributors)
+  * [Contact/Questions](#contact/questions)
+  
+  ### Description:
+  ${data.projectDesc}
+  ${data.projectLink}
+
+  ### Installation:
+  ${data.install}
+
+  ### Usage:
+  ${data.usage}
+  ![alt text](${data.screenshot})
+
+  ### License:
+  Licensed under the ${data.license} license.
+
+  ### Contributors
+  ${data.contribute}
+
+  ### Contact/Questions
+  You can reach me for additional questions at:
+  * GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
+  * Email: ${data.email}
+`;
+};
+
+module.exports = generateMarkdown;
